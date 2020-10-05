84e20ea6789eb08ecdee0333724912aa3d09664d
Spencer Trinh <trinh.kcs85@gmail.com>
2020-10-05 00:38:05 -0400

    added read_FASTA import

--- rosalind/stronghold/import_template.py
+++ rosalind/stronghold/import_template.py
@@ -6,7 +6,7 @@ parent_path = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__
 
 sys.path.append(parent_path)
 from bio_seq import bio_seq
-from utils import readTextFile
+from utils import readTextFile, read_FASTA
 
 # print(os.listdir('.')) # this shows that now the cwd is in the parent dir due
 # to parent_path insert
