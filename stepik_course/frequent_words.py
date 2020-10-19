f59d5cc34204db60cae4b9d67592dc1aadac26c6
Spencer Trinh <trinh.kcs85@gmail.com>
2020-10-18 23:11:17 -0400

    add additional comments for more information to this challenge

--- stepik_course/frequent_words.py
+++ stepik_course/frequent_words.py
@@ -14,7 +14,6 @@ def frequent_codons(seq, k):
         else:
             cnt_kmer[kmer] = 1
 
-
     return cnt_kmer
 
 def get_max_count(dct):
@@ -36,3 +35,21 @@ res = frequent_codons(SEQ, k)
 max_val = get_max_count(res)
 
 print(' '.join([kmer for kmer,cnt in res.items() if cnt == max_val]))
+
+
+
+# side note: this is O( (n * len(seq)-k + 1)*k ) time complexity
+# bc we are running the loop from 0 to len(seq) -k + 1 times
+# and we have to compare k letters or characters in each iteration
+
+
+# also to note: The chance of any one base occurring next in the
+# sequence is 0.25. The chance of three specified bases occurring
+# is therefore 0.25*0.25+0.25 = 0.015625. The number of trials at
+# which this can occur is given by the length of the sequence minus
+# the length of the k-mer + 1. In this case 540 - 3 + 1 = 538. The
+# event of three specified bases occurring in order would therefore be
+# likely to occur 538*0.01562 = 8.40625 times in a sequence of 540 bases.
+
+# if k = 8; we expect an 8-mer to appear every 4^8 = 65536 nucleotides,
+# so that seeing one occur four times is quite surprising.
